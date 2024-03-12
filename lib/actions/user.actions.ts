e6bf75a4f0f75a1f6280df4f989c8200fac6c2ae
reacton <aliyasin.emir16@gmail.com>
2024-03-12 23:59:01 +0300

    added eslint configuration on index.d.ts file to prevent build bugs

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import User from "../database/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
@@ -51,22 +52,22 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
 
 //DELETE
 export async function deleteUser(clerkId: string) {
-    try {
-      await connectToDatabase();
-  
-      // Find user to delete
-      const userToDelete = await User.findOne({ clerkId });
-  
-      if (!userToDelete) {
-        throw new Error("User not found");
-      }
-  
-      // Delete user
-      const deletedUser = await User.findByIdAndDelete(userToDelete._id);
-      revalidatePath("/");
-  
-      return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
-    } catch (error) {
-      handleError(error);
+  try {
+    await connectToDatabase();
+
+    // Find user to delete
+    const userToDelete = await User.findOne({ clerkId });
+
+    if (!userToDelete) {
+      throw new Error("User not found");
     }
+
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+    revalidatePath("/");
+
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+  } catch (error) {
+    handleError(error);
   }
+}
